004996b4cc0e40736e4fd6c0ba7b8b3fd06158f9
Josh Zimmerman <JoshuaZimmerman@quickenloans.com>
2018-10-19 16:00:09 -0400

    this is my first commit for question 3

--- DevBuild.Assessment2_Question3/Program.cs
+++ DevBuild.Assessment2_Question3/Program.cs
@@ -3,13 +3,112 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DevBuild.Utilities;
 
 namespace DevBuild.Assessment2_Question3
 {
     class Program
     {
+        static List<string> strList = new List<string>();
+        static string[] menuOptions = { "Print List", "Add Items to List", "Search List", "Sort List Alphabetically", "Exit" };
+        static string userResponse;
+        static uint userSelection;
+
         static void Main(string[] args)
         {
+            strList.Add("hyperbole");
+            strList.Add("organ grinder");
+            strList.Add("archaeopteryx");
+            strList.Add("apiphobia");
+
+            //DisplayMenuOptions();
+            //PrintList(strList);
+
+            while(true)
+            {
+                DisplayMenuOptions();
+                while (!uint.TryParse(userResponse, out userSelection) || userSelection < 1 || userSelection > menuOptions.Length)
+                {
+                    userResponse = "";
+                    UserInput.PromptUntilValidEntry($"Please enter a selection from 1 to {menuOptions.Length}: ", ref userResponse, InformationType.Numeric);
+                }
+                
+                switch (userSelection)
+                {
+                    case 1: { PrintList(strList); break; }
+                    case 2: { AddToList(strList); break; }
+                    case 3: { SearchList(strList); break; }
+                    case 4: { SortListAlphabetically(strList); break; }
+                    case 5: { return; }
+                }
+                userResponse = "";
+                userSelection = 0;
+
+            }
+
         }
+
+        public static void DisplayMenuOptions()
+        {
+            for (int i = 0; i < menuOptions.Length; i++)
+            {
+                Console.WriteLine($"{i + 1}.) {menuOptions[i]}");
+            }
+        }
+
+        public static void AddToList(List<string> baseList)
+        {
+            string newItem = "";
+
+            UserInput.PromptUntilValidEntry("Please enter word to add: ", ref newItem);
+
+            if (!String.IsNullOrEmpty(newItem))
+            {
+                baseList.Add(newItem);
+            }
+        }
+
+        public static void PrintList(List<string> listItems)
+        {
+            Console.WriteLine("");
+            foreach (string s in listItems)
+            {
+                Console.WriteLine("     " + s);
+            }
+            Console.WriteLine("");
+        }
+
+        public static List<string> SearchList(List<string> listItems)
+        {
+            List<string> searchResults = new List<string>();
+            string userQuery = "";
+
+            UserInput.PromptUntilValidEntry("Please enter a search term: ", ref userQuery);
+
+            //let's see if we can find exact matches
+            searchResults = listItems.Where<string>(x => x == userQuery).ToList<string>();
+
+            //if entire string wasn't matched, let's check for partials
+            if (searchResults.Count == 0)
+            {
+                foreach (string s in listItems)
+                {
+                    if (s.Contains(userQuery))
+                    {
+                        searchResults.Add(s);
+                    }
+                }
+            }
+            Console.WriteLine("Search results: ");
+            PrintList(searchResults);
+
+            return searchResults;
+        }
+
+        public static void SortListAlphabetically(List<string> baseList)
+        {
+            baseList.Sort();
+        }
+
     }
 }
---
+++ DevBuild.Assessment2_Question3/Utilities/Validation.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace DevBuild.Utilities
+{
+    public enum InformationType { Name = 0, EmailAddress, TelephoneNumber, Date, Alphanumeric, Alphabetical, Numeric, ContainsVowels, ContainsPunctuation, ContainsInvalidPunctuation, CustomField }
+
+    /// <summary>
+    /// This struct is used to hold an entry prompt string, an enum for the type of data being validated, 
+    /// and strings for display if entry is valid or invalid for that type
+    /// </summary>
+    public struct ValidDataCheck
+    {
+        public string DataTypePrompt;
+        public InformationType informationType;
+        public string validDataMessage;
+        public string invalidDataMessage;
+
+        public ValidDataCheck(string dataPrompt, InformationType type, string validMessage, string invalidMessage)
+        {
+            DataTypePrompt = dataPrompt;
+            informationType = type;
+            validDataMessage = validMessage;
+            invalidDataMessage = invalidMessage;
+        }
+    }
+
+    class Validation
+    {
+        private const string nameRegexPattern               = @"^[A-Za-z {2}]{1,29}$";
+        private const string emailRegexPattern              = @"^(([A-Za-z0-9]){1,30})+[@]+[a-zA-Z]{5,10}([.])+([A-Za-z.]{3,5})$";
+        private const string telephoneNumberRegexPattern    = @"^([(]?\d{3})[)-]\d{3}[-]\d{4}$";
+        private const string dateRegexPattern               = @"^(\d\d)+[/]+(\d{2})[/](\d){4}$";
+        private const string numericRegexPattern            = @"^[0-9]{1,30}$";
+        private const string alphabeticalRegexPattern       = @"^([A-Za-z])$";
+        private const string alphanumericRegexPattern       = @"(^([A-Za-z0-9])$";
+        private const string vowelsRegexPattern             = @"[AEIOUaeiou]";
+        private const string punctuationRegexPattern        = @"[?!.-,':;]";
+        private const string invalidPunctuationRegexPattern = @"[=+<>/\][)({}|]";
+
+        //This Dictionary allows a programmer to specify the type of data they're looking to validate, and will provide a default regex pattern 
+        //to check the data against. Default regex patterns are defined above, but I have also allowed the user to add a custom regex pattern 
+        //to this Dictionary using the AddUserGeneratedRegex() method. 
+        private static readonly Dictionary<InformationType, string> InformationRegexes = 
+            new Dictionary<InformationType, string> {   { InformationType.Name, nameRegexPattern },
+                                                        { InformationType.EmailAddress, emailRegexPattern },
+                                                        { InformationType.TelephoneNumber, telephoneNumberRegexPattern },
+                                                        { InformationType.Date, dateRegexPattern },
+                                                        { InformationType.Alphabetical, alphabeticalRegexPattern },
+                                                        { InformationType.Alphanumeric, alphanumericRegexPattern },
+                                                        { InformationType.Numeric, numericRegexPattern },
+                                                        { InformationType.ContainsVowels, vowelsRegexPattern },
+                                                        { InformationType.ContainsPunctuation, punctuationRegexPattern },
+                                                        { InformationType.ContainsInvalidPunctuation, invalidPunctuationRegexPattern }};
+
+        /// <summary>
+        /// This function validates information based on the type provided
+        /// </summary>
+        /// <param name="informationType">This is the information type to validate</param>
+        /// <param name="data">This is the string containing the information itself</param>
+        /// <returns></returns>
+        public static bool ValidateInfo(InformationType informationType, string data)
+        {
+            //use the information type argument to set up a regex with the pattern from our Dictionary
+            Regex r = new Regex(Validation.InformationRegexes[informationType]);
+            return r.IsMatch(data);
+        }
+
+
+        /// <summary>
+        /// Let's make this a bit like OpenGL and make it so the user can define their own custom regex pattern. 
+        /// Adding a custom field is not permanent as of this writing, and the custom pattern will be reset at runtime.
+        /// </summary>
+        /// <param name="customRegexPattern">User-generated regex pattern to be added to our dictionary</param>
+        public static void AddUserGeneratedRegex(string customRegexPattern)
+        {
+            try
+            {
+                if (!InformationRegexes.Keys.Contains(InformationType.CustomField))
+                {
+                    InformationRegexes.Add(InformationType.CustomField, customRegexPattern);
+                }
+                else throw new ArgumentException();
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine("A custom validation field has already been added. Multiple custom exceptions will be something we add down the line, but for now, you'll have to restart the program if you want to overwrite the custom field.");
+            }
+        }
+
+    }
+}
